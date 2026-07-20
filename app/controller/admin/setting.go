e2ec46bc316a4f9b28f0c1586d2968f81523ea70
Deatil <24578855+deatil@users.noreply.github.com>
2026-07-20 23:48:13 +0800

    fix

--- app/controller/admin/setting.go
+++ app/controller/admin/setting.go
@@ -109,12 +109,7 @@ func (this *Setting) Save(ctx fiber.Ctx) error {
             })
     }
 
-    events.DoAction("set_cms_router", map[string]string{
-        "website_cate_url": cateUrl,
-        "website_view_url": viewUrl,
-        "website_tag_url": tagUrl,
-        "website_page_url": pageUrl,
-    }, true)
+    events.DoAction("set_cms_router", data, true)
 
     // 清空缓存
     redis.Storage().Delete("settings")
